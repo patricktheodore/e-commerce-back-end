9ba7ebcae340f6488c5ecba3eb6e019c71c5c586
patricktheodore <patricktheodoresara@gmail.com>
2021-11-17 16:22:23 +0800

    models

--- controllers/index.js
+++ controllers/index.js
@@ -8,3 +8,5 @@ router.use((req, res) => {
 });
 
 module.exports = router;
+
+//no changes req.
--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  },
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  },
+  as: 'tags_for_products'
+})
 
 module.exports = {
   Product,
